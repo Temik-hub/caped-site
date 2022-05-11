f4bb4dd8a7b694353590f4dc2238c3c36040d342
Temik-hub <surshkov.art@gmail.com>
2022-05-11 16:12:26 +0300

    исправляет ошибки

--- src/index.js
+++ src/index.js
@@ -3,30 +3,37 @@ import './styles/main.scss'
 const body = document.querySelector("body"),
   sidebar = body.querySelector(".sidebar__container"),
   toggle = body.querySelector(".sidebar__toggle"),
+  sidebarItem = body.querySelector(".sidebar__navigation"),
   searchBtn = body.querySelector(".header__search-icon"),
   searchInput = body.querySelector(".header__search-input"),
   burger = body.querySelector(".header__burger"),
-  navbar = body.querySelector(".header__navigation"),
-  modeSwitch = body.querySelector(".toggle-switch"),
-  modeText = body.querySelector(".mode-text");
+  burgerline = body.querySelector(".header__burger-line"),
+  navbar = body.querySelector(".header__navigation")
+ 
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar__container--close");
+})
+
+sidebarItem.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar__container--close");
+})
+
+let width = document.documentElement.clientWidth;
+  if (width < 999) {
+  sidebar.classList.add("sidebar__container--close");
+}
+
+searchBtn.addEventListener('click', function() {
+  searchInput.focus();
+})
 
 
-  toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("sidebar__container--close");
-  });
-  searchBtn.addEventListener('click', function() {
-    searchInput.focus();
-  });
+burger.addEventListener('click', () => {
+  navbar.classList.toggle("header__navigation--open")
+  burgerline.classList.toggle("header__burger-line--open")
   
-  burger.addEventListener('click', () => {
-    navbar.classList.toggle("header__navigation--open")
-  });
-
-  modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-      modeText.innerText = "Light Mode"
-    } 
-  });
-  
+  navbar.addEventListener('click', () => {
+    navbar.classList.remove("header__navigation--open")
+    burgerline.classList.remove("header__burger-line--open")
+  })
+})
